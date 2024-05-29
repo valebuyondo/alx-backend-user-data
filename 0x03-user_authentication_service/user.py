37ed711ad05c522899262db9bfc99d8b0b56515c
Buyondo Vale <42058151+valebuyondo@users.noreply.github.com>
2024-05-29 21:08:03 +0300

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,4 +17,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
